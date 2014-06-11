873d64b51f11c6096ac895f35620f91013608641
Rafael Chaves <rafael@abstratt.com>
2014-06-11 13:15:28 -0300

    better way to generate a list

--- com.abstratt.kirra.rest.tests/src/com/abstratt/kirra/rest/tests/InstanceTests.java
+++ com.abstratt.kirra.rest.tests/src/com/abstratt/kirra/rest/tests/InstanceTests.java
@@ -1,7 +1,7 @@
 package com.abstratt.kirra.rest.tests;
 
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import com.abstratt.kirra.Instance;
 
@@ -9,7 +9,6 @@ public class InstanceTests extends AbstractRestTests {
 	public void testCreateInstance() {
 		Instance newInstance = new Instance("expenses", "Expense");
 		newInstance.setValue("amount", 10);
-
 		assertTrue(newInstance.isNew());
 		Instance created = instanceManagement.createInstance(newInstance);
 		assertFalse(created.isNew());
@@ -23,10 +22,10 @@ public class InstanceTests extends AbstractRestTests {
 		Instance retrieved = instanceManagement.getInstance("expenses", "Expense", created.getObjectId(), false);
 		assertEquals(10.0, retrieved.getValue("amount"));
 	}
-	
+
 	public void testGetInstances() {
-		int count = 4;
-		IntStream.range(0, count).forEach(i -> instanceManagement.createInstance(new Instance("expenses", "Expense")));
+		int count = 10;
+		Stream.generate(() -> instanceManagement.createInstance(new Instance("expenses", "Expense"))).limit(count).count();
 		List<Instance> retrieved = instanceManagement.getInstances("expenses", "Expense", false);
 		assertEquals(count, retrieved.size());
 	}
