fa8b9853d8512082c839e1150146cb57090d7d47
Rafael Chaves <rafael@abstratt.com>
2014-06-16 16:26:12 -0300

    removed json component

--- com.abstratt.kirra.json/src/com/abstratt/kirra/json/PropertyJSONRepresentation.java
+++
@@ -1,6 +0,0 @@
-package com.abstratt.kirra.json;
-
-import com.abstratt.kirra.Property;
-
-public class PropertyJSONRepresentation extends Property {
-}
--- com.abstratt.kirra.json/src/com/abstratt/kirra/json/RelatedInstanceJSONRepresentation.java
+++
@@ -1,8 +0,0 @@
-package com.abstratt.kirra.json;
-
-import org.codehaus.jackson.annotate.JsonProperty;
-
-public class RelatedInstanceJSONRepresentation extends InstanceJSONRepresentation {
-    @JsonProperty
-    public String relatedUri;
-}
