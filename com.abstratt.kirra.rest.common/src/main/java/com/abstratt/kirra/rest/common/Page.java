243b46927203cb6375d80c6356e64e66eae30d78
Rafael Chaves <rafael@abstratt.com>
2014-09-05 17:03:35 -0300

    showing number of elements in a page

--- com.abstratt.kirra.rest.common/src/main/java/com/abstratt/kirra/rest/common/Page.java
+++ com.abstratt.kirra.rest.common/src/main/java/com/abstratt/kirra/rest/common/Page.java
@@ -3,11 +3,13 @@ package com.abstratt.kirra.rest.common;
 import java.util.List;
 
 public class Page<T> {
-    public final List<T> contents;
     public final long offset;
-
+    public final long length;
+    public final List<T> contents;
+    
     public Page(List<T> contents) {
         this.contents = contents;
         this.offset = 0;
+        this.length = contents.size();
     }
 }
