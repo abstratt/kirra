e2d541da4aef218974a05ba197ebee4ea02fd550
Rafael Chaves <rafael@abstratt.com>
2020-05-02 19:10:01 -0300

    moved to releng repo

--- new-version.sh
+++
@@ -1,8 +0,0 @@
-# Up the version, apply SNAPSHOT and commit  
-mvn \
-  org.codehaus.mojo:build-helper-maven-plugin:3.1.0:parse-version \
-  versions:set "-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT" -DgenerateBackupPoms=false \
-  org.eclipse.tycho:tycho-versions-plugin:1.7.0::update-eclipse-metadata \
-  scm:checkin "-Dmessage=v\${newVersion}"  
-
-git push
