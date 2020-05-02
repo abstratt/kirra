ce9411f91eb8c7f3fdb64797583aac2affc6e072
Rafael Chaves <rafael@abstratt.com>
2020-05-02 19:03:10 -0300

    2.9.10-SNAPSHOT

--- new-version.sh
+++ new-version.sh
@@ -1,6 +1,6 @@
 # Up the version, apply SNAPSHOT and commit  
 mvn \
   org.codehaus.mojo:build-helper-maven-plugin:3.1.0:parse-version \
-  versions:set "-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}" -DgenerateBackupPoms=false \
+  versions:set "-DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT" -DgenerateBackupPoms=false \
   org.eclipse.tycho:tycho-versions-plugin:1.7.0::update-eclipse-metadata \
-  scm:checkin "-Dmessage=\${newVersion}"
+  scm:checkin "-Dmessage=v\${newVersion}"  
